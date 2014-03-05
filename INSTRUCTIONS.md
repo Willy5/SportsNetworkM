62b630313dd568a9c1853b9d3033d42c3e45855e
Sorendil <sorendil@live.fr>
2014-03-05 17:16:03 +0100

    Revert "Merge branch 'master' of gitlab.ninjafactory.fr.nf:the-fireflies/sportsnetworkm"
    This reverts commit d046a2a031951dacb821ba095f39081fcf1cc64f, reversing
    changes made to 0ad3369c7cfd7c1711648fdc8e51941203e61cd6.

--- INSTRUCTIONS.md
+++
@@ -1,5 +0,0 @@
-Si ce n'est pas déjà fait, pour permettre l'upload de médias :
-
-mkdir web/uploads
-mkdir web/uploads/media
-chmod -R 0777 web/uploads
--- src/TheFireflies/SportBundle/TheFirefliesSportBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace TheFireflies\SportBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TheFirefliesSportBundle extends Bundle
-{
-}
