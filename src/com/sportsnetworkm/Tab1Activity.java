0ad3369c7cfd7c1711648fdc8e51941203e61cd6
Willy5 <_xenix@live.fr>
2014-03-05 16:45:17 +0100

    test repare

--- src/com/sportsnetworkm/Tab1Activity.java
+++ src/com/sportsnetworkm/Tab1Activity.java
@@ -8,7 +8,7 @@ public class Tab1Activity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tab1);
+        setContentView(R.layout.tab2);
     }
  
 }
